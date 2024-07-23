38d85027adca60ee8e8f031c0bfd8ca3f51da496
Tai Doan <taidoan@live.co.uk>
2024-07-23 12:31:59 +0100

    Setup dev scripts and docs

---
+++ config/paths.ts
@@ -0,0 +1,10 @@
+import { join, resolve } from "path";
+
+// Resolving the root path of the project by moving one directory up from the current directory (__dirname)
+export const root: string = resolve(__dirname, "..");
+
+// Joining the root path with the 'src' directory to get the source path
+export const source: string = join(root, "src");
+
+// Joining the source path with the 'styles' directory to get the styles path
+export const styles: string = join(source, "styles");
