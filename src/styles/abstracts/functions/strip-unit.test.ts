cf39afabcde7bfec3425c6649f5677ad1589c53c
Tai Doan <taidoan@live.co.uk>
2024-07-28 10:12:00 +0100

    Rounded rem & em functions to two decimal places

--- src/styles/abstracts/functions/strip-unit.test.ts
+++ src/styles/abstracts/functions/strip-unit.test.ts
@@ -6,7 +6,7 @@ import { compileSassString } from "../../../../scripts/scssTest";
 import { outdent } from "outdent";
 
 describe("@function strip-unit", () => {
-  it("strip a unit from a value", async () => {
+  it("strip px unit from a value", async () => {
     const sass = `
       @use "abstracts/functions/strip-unit" as *;
   
@@ -22,5 +22,47 @@ describe("@function strip-unit", () => {
         }
       `,
     });
+  });
+
+  it("strip rem unit from a value", async () => {
+    const sass = `
+      @use "abstracts/functions/strip-unit" as *;
+
+      .foo {
+        width: strip-unit(5rem);
+      }
+    `;
+
+    const expectedCSS = outdent`
+      .foo {
+        width: 5;
+      }
+    `.trim();
+
+    const result = await compileSassString(sass);
+    const recievedCss = result.css.trim();
+
+    expect(recievedCss).toBe(expectedCSS);
+  });
+
+  it("return value if unitless", async () => {
+    const sass = `
+      @use "abstracts/functions/strip-unit" as *;
+
+      .foo {
+        margin-bottom: strip-unit(10);
+      }
+    `;
+
+    const expectedCSS = outdent`
+      .foo {
+        margin-bottom: 10;
+      }
+    `.trim();
+
+    const result = await compileSassString(sass);
+    const recievedCss = result.css.trim();
+
+    expect(recievedCss).toBe(expectedCSS);
   });
 });
