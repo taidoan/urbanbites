219326bffc41d6369cc19ea351b47bf6fb1754a5
Tai Doan <taidoan@live.co.uk>
2024-08-02 11:04:59 +0100

    Fixed mission statement overlap

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import ExportedImage from 'next-image-export-optimizer'
 import h from "@pages/about/hero.module.scss"
 import j from "@pages/about/journey.module.scss"
-import journeyImage from "@images/about/journey.jpg"
+import journeyImage from "@images/about/journey4k.jpeg"
 import sustainableImage from "@images/about/sustainable.jpg"
 import communityImage from "@images/about/community.jpg"
 import Button from '@/components/Button'
@@ -15,30 +15,33 @@ export const metadata: Metadata = {
 const AboutPage = () => {
  return(
   <main>
-    <section className='about__hero'>
-      <div className={`content-grid content-grid--inc-border`}>
-        <h1 className={h.title}>About Us</h1>
-        <Divider variant='primary' hero/>
-        <p className={h.intro}>Welcome to Urban Bites Cafe! Discover our story and explore the heart of our community-focused cafe.</p>
-      </div>
-    </section>
-    <section className='about__journey'>
-      <div className={`content-grid content-grid--inc-border ${j.missionBlock}`}>
-        <div className={j.missionCard}>
-        <p>At <strong>Urban Bites</strong>, our mission is to create a welcoming <strong>community</strong> space that celebrates <strong>fresh</strong>, <strong>locally-sourced</strong> ingredients, <strong>sustainable</strong> practices, and the joy of <strong>togetherness</strong>.</p>
+    <section className='about__intro-container'>
+      <section className='about__hero'>
+        <div className={`content-grid content-grid--inc-border`}>
+          <h1 className={h.title}>About Us</h1>
+          <Divider variant='primary' hero/>
+          <p className={h.intro}>Welcome to Urban Bites Cafe! Discover our story and explore the heart of our community-focused cafe.</p>
         </div>
-      </div>
-      <div>
-        <div className='content-grid content-grid--half'>
-          <h2 className={j.title}>Our Journey</h2>
-          <Divider />
-          <p>Urban Bites Cafe began as a small neighborhood coffee shop with a big dream: to create a welcoming space where the community could come together over delicious, fresh food and exceptional coffee. </p>
-
-          <p>From our humble beginnings, we've grown into a beloved local gathering spot, always staying true to our roots of quality, sustainability, and community spirit. Every step of our journey has been guided by our passion for creating memorable experiences and fostering connections. </p>
+      </section>
+      <section className='about__mission-statement'>
+        <div className={`content-grid content-grid--inc-border ${j.missionBlock}`}>
+          <div className={j.missionCard}>
+          <p>At <strong>Urban Bites</strong>, our mission is to create a welcoming <strong>community</strong> space that celebrates <strong>fresh</strong>, <strong>locally-sourced</strong> ingredients, <strong>sustainable</strong> practices, and the joy of <strong>togetherness</strong>.</p>
+          </div>
         </div>
-        <ExportedImage src={journeyImage} alt="Our Journey" className={j.image} />
-      </div>
+      </section>
+      <section className='about__journey'>
+        <div>
+          <div className='content-grid content-grid--half'>
+            <h2 className={j.title}>Our Journey</h2>
+            <Divider />
+            <p>Urban Bites Cafe began as a small neighborhood coffee shop with a big dream: to create a welcoming space where the community could come together over delicious, fresh food and exceptional coffee. </p>
 
+            <p>From our humble beginnings, we've grown into a beloved local gathering spot, always staying true to our roots of quality, sustainability, and community spirit. Every step of our journey has been guided by our passion for creating memorable experiences and fostering connections. </p>
+          </div>
+          <ExportedImage src={journeyImage} alt="Our Journey" className={j.image} />
+        </div>
+      </section>
     </section>
     <section className='about__values'>  
       <div className='content-grid content-grid--inc-border'>
