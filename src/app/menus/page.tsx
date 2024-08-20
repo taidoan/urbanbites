4483a418ce5810899cdbdefe2f000b8acaead018
Tai Doan <taidoan@live.co.uk>
2024-08-20 16:40:44 +0100

    Started menu page

--- src/app/menus/page.tsx
+++ src/app/menus/page.tsx
@@ -7,29 +7,7 @@ export const metadata: Metadata = {
 const MenusPage = () => {
   return(
    <main>
-     Menus Page Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam culpa incidunt accusantium asperiores. Quam ex cupiditate ipsam, ullam doloremque tempora. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni molestiae officiis laborum inventore? Mollitia nulla ratione quis iure veniam vero unde placeat odio nihil iusto accusantium sed voluptate, recusandae porro!
-     Impedit eveniet recusandae maxime exercitationem praesentium alias facilis consectetur, unde itaque, molestias tempora, sint harum tempore. Modi hic veritatis consectetur atque, dolore saepe impedit possimus harum dolorum pariatur fugiat a.
-     Recusandae fuga tenetur quisquam necessitatibus illum veritatis architecto tempora alias in minus porro, harum velit corrupti adipisci debitis voluptas asperiores sapiente iste, quae ut, placeat saepe voluptate. Itaque, impedit tempora.
-     Assumenda, quasi veritatis suscipit tenetur necessitatibus atque eos ea porro animi dignissimos voluptatum eius modi sed, qui quo laboriosam nisi! Culpa magnam cupiditate veniam neque soluta dicta modi. Quia, distinctio.
-     Asperiores distinctio similique dolor quod velit maiores deleniti perspiciatis error voluptatibus facere, ipsa, qui quae id minima, voluptates magni earum corrupti laborum libero vero quam amet reiciendis? Eum, ratione id.
-     Quas magni, iusto possimus accusantium, fuga debitis eligendi, molestias nam cupiditate repellendus odio rem dignissimos! Voluptas, fugiat quae voluptatum ipsum aliquam aut veritatis eveniet facilis dolor corporis architecto inventore. Et!
-     Ipsa, nesciunt suscipit aliquam corrupti, nostrum earum qui optio quisquam tempore culpa harum totam perspiciatis! Eum amet atque laudantium nulla debitis, libero tenetur delectus minus eaque suscipit? Commodi, perferendis facere?
-     Voluptatibus ad mollitia dolorem, eveniet modi eum deserunt necessitatibus culpa quis ut dolore repellat incidunt aspernatur laborum aut tempore, eius, quas at saepe odio natus iusto. Saepe culpa quia fugiat!
-     Illum, laboriosam provident. Nemo illum numquam error aliquid deserunt aspernatur, sed dicta laboriosam molestias ducimus? Maiores numquam veniam vero nulla, tempora blanditiis voluptatem et, doloremque ex facilis commodi fugit pariatur!
-     Ullam, fuga perferendis dolor consequuntur qui laborum, earum consequatur autem optio ratione corrupti unde eligendi eius eaque at. Repellendus reprehenderit omnis aliquam eligendi obcaecati voluptatum accusamus modi earum officia numquam!
-     Nisi reprehenderit, consequuntur maiores molestias laborum soluta assumenda eveniet ipsam possimus doloremque culpa explicabo quibusdam perspiciatis totam quaerat aliquam itaque cupiditate? Fuga, soluta pariatur ex nemo dolores quisquam voluptatum neque!
-     Eaque fuga rem, architecto praesentium perferendis nobis officiis fugit nam. Id non, animi, magni, facilis fugit inventore nihil nulla ea officiis dignissimos cumque molestias accusamus eius sequi perferendis laboriosam? Tempore.
-     Corrupti accusantium repudiandae ut hic mollitia ratione, obcaecati quod sed animi explicabo aliquid sunt sint, ipsum nemo officia tempora impedit harum voluptates quis ullam. Suscipit, soluta. Ipsum nostrum quae dicta!
-     A consequuntur dolores quasi deleniti et. Vero cupiditate, consectetur quaerat minima placeat vel tempore dolore eum tempora facere, fuga consequuntur a voluptatem eaque, repudiandae minus nihil. Aliquam accusamus fugit veritatis.
-     Quisquam cum magni praesentium provident iure nobis asperiores itaque explicabo modi earum labore voluptatem autem, illo blanditiis quis nesciunt expedita non qui velit consectetur unde tempore fugit ducimus? Odit, beatae?
-     Similique harum porro quaerat exercitationem quibusdam. Quae, molestias vitae quaerat deleniti expedita saepe placeat hic obcaecati eligendi repudiandae id nostrum adipisci rerum deserunt sunt similique voluptates accusamus distinctio aliquid? Dolor.
-     Maxime sapiente harum quis et tenetur? Nemo officia sed ad dolorem beatae vel tenetur, ipsa aspernatur nihil, explicabo dicta! Maxime beatae possimus, ducimus veniam illo pariatur consequuntur et nisi facilis.
-     Sapiente ipsum magnam facere, cupiditate magni nihil, blanditiis facilis quaerat impedit itaque delectus rem minima voluptatem natus aut officia! Beatae magni voluptas aspernatur unde sed minima possimus magnam sint asperiores!
-     Quasi nesciunt quaerat, nostrum esse minima enim fugit adipisci fugiat? Quia sequi optio accusantium ipsum quae laboriosam saepe quaerat natus. Magni, corrupti? Dignissimos similique error dolorum. Rem eum ad animi?
-     Quod, nam exercitationem est tempora reiciendis rerum, voluptas magni totam neque et voluptate earum suscipit ipsam alias aliquam deleniti harum! Veritatis, ratione tenetur excepturi nesciunt provident libero recusandae molestias earum.
-     Nam, dolore. Sapiente deserunt reprehenderit laudantium omnis amet dolorem, nulla officia ipsa iusto eligendi optio aspernatur, doloremque esse tempora maxime nobis vitae eius perspiciatis temporibus quos consequatur ut quo corporis?
-     Culpa ipsum illo, id sint est repellendus sit beatae omnis quod? Assumenda beatae ducimus aspernatur. Hic deleniti libero excepturi, dolorem dicta accusamus quo accusantium consectetur inventore et soluta natus temporibus!
-     Ad obcaecati optio ex nam voluptatibus totam iure, possimus excepturi eum, harum dolores animi nisi aspernatur suscipit atque deserunt consequuntur iusto dolor amet architecto vitae officiis aliquid iste? Quam, harum.
+     Menu page
    </main>
   )
  }
