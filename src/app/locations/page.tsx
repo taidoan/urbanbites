beb5517273aca5cf4ec6d1c2078644fa7a0dc12c
Tai Doan <taidoan@live.co.uk>
2024-10-06 13:51:23 +0100

    Started locaitons card

--- src/app/locations/page.tsx
+++ src/app/locations/page.tsx
@@ -1,5 +1,9 @@
 import Divider from "@/components/Divider"
 import type { Metadata } from "next";
+import Card from "@/components/Card";
+import CardImage from "@/components/Card/CardImage"
+import CardBody from "@/components/Card/CardBody"
+import { Locations } from "@/content/locations";
 
 export const metadata: Metadata = {
   title: 'Urban Bites | Bookings'
@@ -21,6 +25,14 @@ const LocationsPage = () => {
           <h2>Where We Are</h2>
           <Divider center={true} />
           <p>Each Urban Bites offers a unique yet welcoming atmosphere—find your nearest café to enjoy our delicious offerings and exceptional service.</p>
+        </div>
+        <div>
+          {Locations.map(location => (
+            <Card key={location.id}>
+              {location.name}
+              <CardImage src={location.image} alt="Alt texty" imageSize="large"/>
+            </Card>
+          ))}
         </div>
       </div>
      </section>
