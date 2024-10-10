b751216072da528877a987e091d5e57265524a9c
Tai Doan <taidoan@live.co.uk>
2024-10-10 12:00:09 +0100

    Moved menu content to contents folder

--- src/app/menus/menu.tsx
+++ src/app/menus/menu.tsx
@@ -1,30 +1,14 @@
 'use client';
 import { useState, useEffect } from "react";
 import Filter from "@/components/Filter";
-import { Categories } from "./content/categories";
-import { MenuItems } from "./content/items";
+import { Categories, MenuItems, DrinkCategories } from "@/content/menu";
 import { Item } from "@/components/Filter/types";
 import MenuItemCard from "./MenuItemCard"
 import styles from "@/styles/pages/menus/content.module.scss"
 import useMediaQuery from "@/hooks/useMediaQuery";
 import Divider from "@/components/Divider";
-type ItemProps = {
-  items: Item[],
-  currentTab: string,
-}
 
-const drinkCategoryDescriptions: Record<string, string> = {
-  "chocolate": "Indulge in our rich and creamy chocolate drinks, crafted to perfection.",
-  "tea": "Explore a variety of fine teas, each brewed to bring out the unique flavors.",
-  "coffee": "Savor our expertly brewed coffee, made from the finest roasted beans.",
-  "soft drinks": "Refresh yourself with our selection of chilled, carbonated beverages.",
-  "fruit juice": "Enjoy the natural sweetness of freshly squeezed fruit juices.",
-  "beer & cider": "Discover our handpicked selection of craft beers and ciders.",
-  "wine": "Curated wines to complement your meal, perfect for any occasion.",
-  "smoothies": "A refreshing blend of fruits and yogurt, perfect for a healthy treat."
-};
-
-const FilterItems = ({ items, currentTab }: ItemProps) => {
+const FilterItems = ({ items, currentTab }: { items: Item[], currentTab: string}) => {
   const filteredItems = items.filter(item => item.category === currentTab);
 
   const groupedItems = filteredItems.reduce((groups, item) => {
@@ -44,7 +28,7 @@ const FilterItems = ({ items, currentTab }: ItemProps) => {
             <div>
               <h2 className={`${styles.title} ${styles.titleSmall}`}>{drinkCategory}</h2>
               <Divider />
-              <p>{drinkCategoryDescriptions[drinkCategory] || "No description available."}</p>
+              <p>{DrinkCategories [drinkCategory] || "No description available."}</p>
             </div>
             <MenuItemCard items={groupedItems[drinkCategory]} />
           </div>
--- src/content/menu.ts
+++ src/content/menu.ts
@@ -1,4 +1,72 @@
-import { Item } from "@/components/Filter/types";
+import { Tab, Item } from "@/components/Filter/types";
+
+export const Categories: Tab[] = [
+  {
+    id: "breakfast",
+    label: "Breakfasts",
+    description:
+      "Start your morning right with our hearty breakfast options. From fluffy pancakes to savory omelets, there's something for everyone.",
+  },
+  {
+    id: "starters",
+    label: "Starters",
+    description:
+      "Begin your meal with our flavorful starters, perfect for sharing. Enjoy a variety of light bites, from bruschetta to creamy soups.",
+  },
+  {
+    id: "salads",
+    label: "Salads",
+    description:
+      "Fresh, crisp, and full of flavor, our salads are a healthy delight. Choose from classic favorites or adventurous new combinations.",
+  },
+  {
+    id: "pasta",
+    label: "Pasta",
+    description:
+      "Our pastas bring comfort and flavor to your table. From creamy Alfredo to rich Bolognese, each dish is crafted with care.",
+  },
+  {
+    id: "deli",
+    label: "Deli",
+    description:
+      "Savor our deli selections, featuring premium sandwiches and charcuterie boards. Perfect for a quick bite or a relaxed meal.",
+  },
+  {
+    id: "mains",
+    label: "Mains",
+    description:
+      "Savor the heart of our cuisine with our carefully crafted main meals. Each dish is a symphony of flavors, designed to satisfy and delight.",
+  },
+  {
+    id: "desserts",
+    label: "Desserts",
+    description:
+      "Indulge in our irresistible desserts, the perfect ending to any meal. Enjoy classic sweets like cheesecake and rich chocolate cake.",
+  },
+  {
+    id: "drinks",
+    label: "Drinks",
+    description:
+      "Refresh with our wide range of drinks, from soft beverages to fine wines. Whether a quick coffee or a crafted cocktail, we have it all.",
+  },
+];
+
+export const DrinkCategories: Record<string, string> = {
+  chocolate:
+    "Indulge in our rich and creamy chocolate drinks, crafted to perfection.",
+  tea: "Explore a variety of fine teas, each brewed to bring out the unique flavors.",
+  coffee:
+    "Savor our expertly brewed coffee, made from the finest roasted beans.",
+  "soft drinks":
+    "Refresh yourself with our selection of chilled, carbonated beverages.",
+  "fruit juice":
+    "Enjoy the natural sweetness of freshly squeezed fruit juices.",
+  "beer & cider":
+    "Discover our handpicked selection of craft beers and ciders.",
+  wine: "Curated wines to complement your meal, perfect for any occasion.",
+  smoothies:
+    "A refreshing blend of fruits and yogurt, perfect for a healthy treat.",
+};
 
 export const MenuItems: Item[] = [
   {
