f2e8cf63be4342d9f6a5942d0946ce36e5e9bfba
Tai Doan <taidoan@live.co.uk>
2025-09-19 10:26:31 +0100

    fix(card): Fixed type for card image.

--- src/app/about/components/swiper.tsx
+++ src/app/about/components/swiper.tsx
@@ -5,7 +5,15 @@ import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import useMediaQuery from "@/hooks/useMediaQuery";
-import { Card, CardTitle, CardBody, CardImage, CardCTA, CardDate, Review } from "./index";
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  CardImage,
+  CardCTA,
+  CardDate,
+  Review,
+} from "./index";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
@@ -51,52 +59,124 @@ type SwiperSectionProps = {
   name?: string;
 };
 
-const ImageLink = ({ src, alt, url, imageSize }: { src: string | StaticImageData | undefined, alt: string, url?: string, imageSize?: string }) => (
+const ImageLink = ({
+  src,
+  alt,
+  url,
+  imageSize,
+}: {
+  src: string | StaticImageData | undefined;
+  alt: string;
+  url?: string;
+  imageSize?: string;
+}) =>
   url ? (
     <Link href={url} className="imageLink">
-      <CardImage src={src} alt={alt} imageSize={imageSize} />
+      <CardImage src={src ?? ""} alt={alt} imageSize={imageSize} />
     </Link>
   ) : (
-    <CardImage src={src} alt={alt} imageSize={imageSize} />
-  )
-);
+    <CardImage src={src ?? ""} alt={alt} imageSize={imageSize} />
+  );
 
-const TitleLink = ({ title, size, url }: { title: string, size?: "large" | "small", url?: string }) => (
+const TitleLink = ({
+  title,
+  size,
+  url,
+}: {
+  title: string;
+  size?: "large" | "small";
+  url?: string;
+}) =>
   url ? (
     <Link href={url} className="titleLink">
       <CardTitle title={title} size={size} />
     </Link>
   ) : (
     <CardTitle title={title} size={size} />
-  )
-);
+  );
 
-const CardContent = ({ slide, event, review, insideImage, imageSize, centered }: { slide: Slide, event: boolean, review: boolean, insideImage: boolean, imageSize: string, centered: boolean }) => (
+const CardContent = ({
+  slide,
+  event,
+  review,
+  insideImage,
+  imageSize,
+  centered,
+}: {
+  slide: Slide;
+  event: boolean;
+  review: boolean;
+  insideImage: boolean;
+  imageSize: string;
+  centered: boolean;
+}) => (
   <Card event={event} review={review}>
-    {event && slide.date && <CardDate day={slide.date.day} month={slide.date.monthText} />}
+    {event && slide.date && (
+      <CardDate day={slide.date.day} month={slide.date.monthText} />
+    )}
     {insideImage && slide.image && !review ? (
       <CardBody textCentre={centered}>
-        <ImageLink src={slide.image.src} alt={slide.image.alt} url={slide.url} imageSize={imageSize} />
-        {slide.title && <TitleLink title={slide.title.title} size={slide.title.size} url={slide.url} />}
+        <ImageLink
+          src={slide.image.src}
+          alt={slide.image.alt}
+          url={slide.url}
+          imageSize={imageSize}
+        />
+        {slide.title && (
+          <TitleLink
+            title={slide.title.title}
+            size={slide.title.size}
+            url={slide.url}
+          />
+        )}
         <p>{slide.content}</p>
         {slide.cta && (
-          <CardCTA title={slide.cta.title} variant={slide.cta.variant} href={slide.cta.href} className={slide.cta.className} target={slide.cta.target} />
+          <CardCTA
+            title={slide.cta.title}
+            variant={slide.cta.variant}
+            href={slide.cta.href}
+            className={slide.cta.className}
+            target={slide.cta.target}
+          />
         )}
       </CardBody>
     ) : review && slide.review ? (
       <CardBody>
-        <Review rating={slide.review.rating} author={slide.review.author} date={slide.review.date}>
+        <Review
+          rating={slide.review.rating}
+          author={slide.review.author}
+          date={slide.review.date}
+        >
           <p>{slide.content}</p>
         </Review>
       </CardBody>
     ) : (
       <>
-        {!insideImage && slide.image && <ImageLink src={slide.image.src} alt={slide.image.alt} url={slide.url} imageSize={imageSize} />}
+        {!insideImage && slide.image && (
+          <ImageLink
+            src={slide.image.src}
+            alt={slide.image.alt}
+            url={slide.url}
+            imageSize={imageSize}
+          />
+        )}
         <CardBody textCentre={centered}>
-          {slide.title && <TitleLink title={slide.title.title} size={slide.title.size} url={slide.url} />}
+          {slide.title && (
+            <TitleLink
+              title={slide.title.title}
+              size={slide.title.size}
+              url={slide.url}
+            />
+          )}
           <p>{slide.content}</p>
           {slide.cta && (
-            <CardCTA title={slide.cta.title} variant={slide.cta.variant} href={slide.cta.href} className={slide.cta.className} target={slide.cta.target}  />
+            <CardCTA
+              title={slide.cta.title}
+              variant={slide.cta.variant}
+              href={slide.cta.href}
+              className={slide.cta.className}
+              target={slide.cta.target}
+            />
           )}
         </CardBody>
       </>
@@ -104,18 +184,27 @@ const CardContent = ({ slide, event, review, insideImage, imageSize, centered }:
   </Card>
 );
 
-const SwiperSection = ({ slides, event = false, review = false, insideImage = true, imageSize = 'normal', centered = false, name }: SwiperSectionProps) => {
+const SwiperSection = ({
+  slides,
+  event = false,
+  review = false,
+  insideImage = true,
+  imageSize = "normal",
+  centered = false,
+  name,
+}: SwiperSectionProps) => {
   const isDesktop = useMediaQuery("(min-width: 85.1875em)");
   const isSmaller = useMediaQuery("(min-width: 75rem)");
   const [showSwiper, setShowSwiper] = useState(true);
 
   useEffect(() => {
-    const shouldInit = (name === 'events' || name === 'locations') ? !isSmaller : !isDesktop;
+    const shouldInit =
+      name === "events" || name === "locations" ? !isSmaller : !isDesktop;
     setShowSwiper(shouldInit);
   }, [isDesktop, isSmaller, name]);
 
   return showSwiper ? (
-    <Swiper 
+    <Swiper
       modules={[Pagination]}
       spaceBetween={32}
       slidesPerView={1}
@@ -124,12 +213,19 @@ const SwiperSection = ({ slides, event = false, review = false, insideImage = tr
       breakpoints={{
         780: { slidesPerView: 2 },
         1200: { slidesPerView: 3 },
-        1363: { slidesPerView: 4 }
+        1363: { slidesPerView: 4 },
       }}
     >
       {slides.map((slide, index) => (
         <SwiperSlide key={index}>
-          <CardContent slide={slide} event={event} review={review} insideImage={insideImage} imageSize={imageSize} centered={centered} />
+          <CardContent
+            slide={slide}
+            event={event}
+            review={review}
+            insideImage={insideImage}
+            imageSize={imageSize}
+            centered={centered}
+          />
         </SwiperSlide>
       ))}
     </Swiper>
@@ -137,7 +233,14 @@ const SwiperSection = ({ slides, event = false, review = false, insideImage = tr
     <div className={`${name}__swiper`}>
       {slides.map((slide, index) => (
         <div key={index} className="swiper-slide">
-          <CardContent slide={slide} event={event} review={review} insideImage={insideImage} imageSize={imageSize} centered={centered} />
+          <CardContent
+            slide={slide}
+            event={event}
+            review={review}
+            insideImage={insideImage}
+            imageSize={imageSize}
+            centered={centered}
+          />
         </div>
       ))}
     </div>
--- src/app/events/components/EventCard.tsx
+++ src/app/events/components/EventCard.tsx
@@ -1,29 +1,35 @@
-import style from './styles.module.scss'
-import { Event } from '@/content/types'
-import Card from '@/components/Card'
-import CardTitle from '@/components/Card/CardTitle'
-import CardImage from '@/components/Card/CardImage'
-import CardBody from '@/components/Card/CardBody'
-import CardDate from '@/components/Card/CardDate'
-import Divider from '@/components/Divider'
-import EventInfo from './EventInfo'
-import classNames from 'classnames'
+import style from "./styles.module.scss";
+import { Event } from "@/content/types";
+import Card from "@/components/Card";
+import CardTitle from "@/components/Card/CardTitle";
+import CardImage from "@/components/Card/CardImage";
+import CardBody from "@/components/Card/CardBody";
+import CardDate from "@/components/Card/CardDate";
+import Divider from "@/components/Divider";
+import EventInfo from "./EventInfo";
+import classNames from "classnames";
 
 type EventCardProps = {
   className?: string;
   entry: Event;
-}
+};
 
-const EventCard = ({className, entry}: EventCardProps) => {
+const EventCard = ({ className, entry }: EventCardProps) => {
   const eventCardClasses = classNames(style.eventCard, className);
 
-  return(
+  return (
     <Card id={`${entry.name}`} event={true} className={eventCardClasses}>
-      {typeof entry.date === 'object' ? (
+      {typeof entry.date === "object" ? (
         <CardDate day={entry.date.day} month={entry.date.monthText} />
-      ) : (<></>)}
+      ) : (
+        <></>
+      )}
       <CardBody className={style.eventCardBody}>
-        <CardImage src={entry.image} alt={entry.name} classes={style.eventImage} />
+        <CardImage
+          src={entry.image ?? ""}
+          alt={entry.name}
+          classes={style.eventImage}
+        />
         <div className={style.eventCardContent}>
           <CardTitle title={entry.name} />
           <Divider weight="thin" variant="grey" width="full-width" />
@@ -36,7 +42,7 @@ const EventCard = ({className, entry}: EventCardProps) => {
         </div>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default EventCard
+export default EventCard;
